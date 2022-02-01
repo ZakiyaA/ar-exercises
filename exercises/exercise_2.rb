34dfeee86b92782be5a76c3637377e13d35dcb4f
ZakiyaA <zakiyaalfughi@gmail.com>
2022-02-01 22:44:37 +0000

    update exercise_1 functionality

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.where(name: 'Richmond')
+@store1.update(name: 'Whitby')
+
+
+puts "store1------------"
+p @store1
+puts "Store2------------"
+p @store2
